6dccff75c75eea6f6654fef64a304812952a27c3
Aaron G <mastaag@gmail.com>
2020-01-16 01:32:48 +0100

    jjd: Fix comment in config.h

--- config.h
+++ config.h
@@ -7,7 +7,7 @@
 /* Port used when IRC_PORT var is empty */
 #define DEFAULT_PORT "6667"
 
-/* Command used as parser when IRC_PARSER is empty */
+/* Command used as parser when IRC_CLIENT is empty */
 #define DEFAULT_CMD "jjc"
 
 /* The name of the FIFO */
